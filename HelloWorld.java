9cc7163bdba5cbdd6a23941135da9788a0c93a02
Marko Matic <maticmarko95@yahoo.com>
2021-11-03 15:50:25 +0100

    Hook test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Zdravo svete!");
+    System.out.println("Hello world!");
   }
 }
